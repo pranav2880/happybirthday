9817ff1b14a2dc8ae4886537bf5f90f4954f1363
pranav2880 <rajpranav288@gmail.com>
2026-01-03 18:10:52 +0530

    Fix route require casing for Render

--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Photo = require('../models/Photo');
+const Photo = require('../models/photo');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
